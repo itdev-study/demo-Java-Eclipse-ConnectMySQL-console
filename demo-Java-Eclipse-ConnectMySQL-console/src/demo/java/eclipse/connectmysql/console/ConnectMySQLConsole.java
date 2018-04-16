cae11d2643decd95a19787a35865a595cc9bec79
root <root@ub16-base>
2018-04-16 14:01:15 +0300

    Test Alpha-1.0

--- demo-Java-Eclipse-ConnectMySQL-console/src/demo/java/eclipse/connectmysql/console/ConnectMySQLConsole.java
+++ demo-Java-Eclipse-ConnectMySQL-console/src/demo/java/eclipse/connectmysql/console/ConnectMySQLConsole.java
@@ -2,18 +2,18 @@ package demo.java.eclipse.connectmysql.console;
 
 import java.sql.Connection;
 import java.sql.DriverManager;
+import java.sql.ResultSet;
 import java.sql.SQLException;
+import java.sql.Statement;
 
 public class ConnectMySQLConsole {
 
-	public static void main(String[] args) {
-		// TODO Auto-generated method stub
+	public static void main(String[] args) {		
 		System.out.println("Test Connection to MySQL");
 		try {
 			Class.forName("com.mysql.jdbc.Driver");
 			System.out.println("Driver loaded");
 		} catch (ClassNotFoundException e) {
-			// TODO Auto-generated catch block
 			System.out.println("Error loading jdbc driver");
 			e.printStackTrace();
 		}
@@ -27,16 +27,30 @@ public class ConnectMySQLConsole {
 			con=DriverManager.getConnection(url, user, password);
 			System.out.println("Connection to DB created");
 			
+			Statement statment = con.createStatement();
+			System.out.println("Statement created");
+			
+			//String insertSQL = "insert into items values (1, 'TEST-555')";			
+			//statment.executeUpdate(insertSQL);		
+			
+			 String sql;
+		     sql = "SELECT id, name FROM items";
+		     ResultSet rs = statment.executeQuery(sql);
+			
+		     while(rs.next()){		       
+		         int id  = rs.getInt("id");
+		         String name = rs.getString("name");  
+		         System.out.print("ID: " + id);		        
+		         System.out.println("Name" + name);
+		      }		      
+		      rs.close();
+		      statment.close();
+		      con.close();
+		      
 		} catch (SQLException e) {
 			// TODO Auto-generated catch block
 			e.printStackTrace();
 		}
-		
-		
-		
-		
-		
-		
 
 	}
 
