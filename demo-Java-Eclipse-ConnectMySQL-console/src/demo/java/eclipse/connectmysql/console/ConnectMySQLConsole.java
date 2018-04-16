9b4a63ec5418445ad56d34433d701480d5037dcf
root <root@ub16-base>
2018-04-16 11:50:52 +0300

    Connect To MySQL from Java console v. Alpha-0.1

---
+++ demo-Java-Eclipse-ConnectMySQL-console/src/demo/java/eclipse/connectmysql/console/ConnectMySQLConsole.java
@@ -0,0 +1,11 @@
+package demo.java.eclipse.connectmysql.console;
+
+public class ConnectMySQLConsole {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		System.out.println("Test Connection to MySQL");
+
+	}
+
+}
